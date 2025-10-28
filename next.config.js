be374df1d0e81658fd56bbd01c1d646fa332775b
mountechcorp <mountechcorp@gmail.com>
2025-10-28 16:40:14 -0400

    test

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
-// next.config.js
-const isCI = process.env.CI === 'true'; // Render sets CI=true
 module.exports = {
   typescript: {
     // Allow production builds to succeed even if there are TS errors
-    ignoreBuildErrors: isCI,
+    ignoreBuildErrors: true,
   },
   eslint: {
     // Optional: don’t fail build on ESLint issues in CI
-    ignoreDuringBuilds: isCI,
+    ignoreDuringBuilds: true,
   },
 };
